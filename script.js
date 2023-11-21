fce66d3742f3dda0d6fe22a881ce9ca77bbeaa11
lucasmfs23 <142855185+lucasmfs23@users.noreply.github.com>
2023-11-21 14:32:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function valor(){
         for (var i = 1; i <= x; i++) {
           var n= n+ "O número "+i+" é";
             if(i % 2==0){
-                n=n + " par"+"<br>";
+                n=n + " par."+"<br>";
             }else{
-                n=n + " ímpar"+"<br>";
+                n=n + " ímpar."+"<br>";
                 
             } document.getElementById("resultado").innerHTML =n; 
         }   
